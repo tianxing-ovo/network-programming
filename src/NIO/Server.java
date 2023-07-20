dba429e59a48ce0276d21f10c1237d3b0ff29353
tianxing-ovo <101339843+tianxing-ovo@users.noreply.github.com>
2023-07-20 10:59:37 +0800

    Update Server.java

--- src/NIO/Server.java
+++ src/NIO/Server.java
@@ -1,14 +1,28 @@
 package NIO;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 
+/**
+ * 服务端
+ */
 public class Server {
     public static void main(String[] args) throws IOException {
         //创建serverSocketChannel对象
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        ServerSocketChannel ssc = ServerSocketChannel.open();
         //设置为非阻塞的方式
-        serverSocketChannel.configureBlocking(false);
+        ssc.configureBlocking(false);
+        //设置服务器端口号
+        ssc.socket().bind(new InetSocketAddress(9000));
+        //创建selector多路复用器
+        Selector selector = Selector.open();
+        //把ServerSocketChannel注册到selector上,并且对接收请求感兴趣
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+        while (true) {
 
+        }
     }
 }
