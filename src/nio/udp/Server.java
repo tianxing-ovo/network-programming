db8033c48371548cd59c941e3b4394761342a364
ltx <2050365570@qq.com>
2023-11-11 22:51:21 +0800

    add nio udp client and server

--- src/nio/udp/Server.java
+++ src/nio/udp/Server.java
@@ -4,18 +4,28 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 
 @Slf4j
+@SuppressWarnings("InfiniteLoopStatement")
 public class Server {
-    public static void main(String[] args) throws IOException {
-        DatagramChannel channel = DatagramChannel.open();
-        channel.bind(new InetSocketAddress(9000));
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        channel.receive(buffer);
-        buffer.flip();
-        log.info("服务端收到数据:{}", new String(buffer.array(), 0, buffer.limit()));
-        channel.close();
+    public static void main(String[] args) {
+        try (DatagramChannel channel = DatagramChannel.open()) {
+            channel.bind(new InetSocketAddress(9000));
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while (true) {
+                // 读取客户端发送的数据
+                buffer.clear();
+                SocketAddress socketAddress = channel.receive(buffer);
+                buffer.flip();
+                log.info("读取到客户端的数据:{}", new String(buffer.array(), 0, buffer.limit()));
+                // 向客户端返回数据
+                channel.send(ByteBuffer.wrap("hello datagram channel client".getBytes()), socketAddress);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
