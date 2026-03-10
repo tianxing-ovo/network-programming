9fd54f82a5aa84f0c30fb0c9d4f5d16a29a90fc1
20503 <2050365570@qq.com>
2026-03-10 15:19:31 +0800

    feat(nio): 添加只读缓冲区和子缓冲区示例代码
    - 创建ReadOnlyBuffer类，演示只读缓冲区的创建与共享底层数据
    - 实现SubBuffer类，展示子缓冲区的创建和数据共享
    - 说明子缓冲区position和limit的值及其操作范围
    - 通过修改子缓冲区，验证原始缓冲区数据同步变化
    - 使用注

---
+++ src/main/java/nio/ReadOnlyBuffer.java
@@ -0,0 +1,21 @@
+package nio;
+
+import java.nio.ByteBuffer;
+
+/**
+ * 只读缓冲区
+ *
+ * @author tianxing
+ */
+public class ReadOnlyBuffer {
+    public static void main(String[] args) {
+        // 创建原始缓冲区
+        ByteBuffer buffer = ByteBuffer.wrap(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        // 创建只读缓冲区(共享相同的底层数据数组)
+        ByteBuffer readOnlyBuffer = buffer.asReadOnlyBuffer();
+        // 修改原始缓冲区的数据
+        buffer.put(0, (byte) 40);
+        // 40
+        System.out.println(readOnlyBuffer.get(0));
+    }
+}
---
+++ src/main/java/nio/SubBuffer.java
@@ -0,0 +1,31 @@
+package nio;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+/**
+ * 子缓冲区
+ *
+ * @author tianxing
+ */
+public class SubBuffer {
+    public static void main(String[] args) {
+        // 创建原始缓冲区
+        ByteBuffer buffer = ByteBuffer.wrap(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        // 设置可操作数据范围: [3,7)
+        buffer.position(3);
+        buffer.limit(7);
+        // 创建子缓冲区(共享相同的底层数据数组)
+        ByteBuffer subBuffer = buffer.slice();
+        // 子缓冲区的position: 0
+        System.out.println("子缓冲区的position: " + subBuffer.position());
+        // 子缓冲区的limit: 4
+        System.out.println("子缓冲区的limit: " + subBuffer.limit());
+        // 更改子缓冲区的内容(原始缓冲区的内容也会改变)
+        subBuffer.put(0, (byte) 40);
+        // 原始缓冲区的内容: [1, 2, 3, 40, 5, 6, 7, 8, 9]
+        System.out.println("原始缓冲区的内容: " + Arrays.toString(buffer.array()));
+        // 子缓冲区的内容: [1, 2, 3, 40, 5, 6, 7, 8, 9]
+        System.out.println("子缓冲区的内容: " + Arrays.toString(subBuffer.array()));
+    }
+}
